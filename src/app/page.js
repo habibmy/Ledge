564ca646dc5fb71d3047555bfc0efdc70b36e3c2
habibmy <habib.my@rediffmail.com>
2023-08-01 19:23:36 +0530

    added layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,175 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main class="ml-60 pt-16 max-h-screen overflow-auto">
+      <div class="px-6 py-8">
+        <div class="max-w-4xl mx-auto">
+          <div class="bg-white rounded-3xl p-8 mb-5">
+            <h1 class="text-3xl font-bold mb-10">
+              Messaging ID framework development for the marketing branch
+            </h1>
+            <div class="flex items-center justify-between">
+              <div class="flex items-stretch">
+                <div class="text-gray-400 text-xs">
+                  Members
+                  <br />
+                  connected
+                </div>
+                <div class="h-100 border-l mx-4"></div>
+                <div class="flex flex-nowrap -space-x-3">
+                  <div class="h-9 w-9">
+                    <img
+                      class="object-cover w-full h-full rounded-full"
+                      src="https://ui-avatars.com/api/?background=random"
+                    />
+                  </div>
+                  <div class="h-9 w-9">
+                    <img
+                      class="object-cover w-full h-full rounded-full"
+                      src="https://ui-avatars.com/api/?background=random"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div class="flex items-center gap-x-2">
+                <button
+                  type="button"
+                  class="inline-flex items-center justify-center h-9 px-3 rounded-xl border hover:border-gray-400 text-gray-800 hover:text-gray-900 transition"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="1em"
+                    height="1em"
+                    fill="currentColor"
+                    class="bi bi-chat-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9.06 9.06 0 0 0 8 15z" />
+                  </svg>
+                </button>
+                <button
+                  type="button"
+                  class="inline-flex items-center justify-center h-9 px-5 rounded-xl bg-gray-900 text-gray-300 hover:text-white text-sm font-semibold transition"
+                >
+                  Open
+                </button>
+              </div>
+            </div>
+
+            <hr class="my-10" />
+
+            <div class="grid grid-cols-2 gap-x-20">
+              <div>
+                <h2 class="text-2xl font-bold mb-4">Stats</h2>
+
+                <div class="grid grid-cols-2 gap-4">
+                  <div class="col-span-2">
+                    <div class="p-4 bg-green-100 rounded-xl">
+                      <div class="font-bold text-xl text-gray-800 leading-none">
+                        Good day, <br />
+                        Kristin
+                      </div>
+                      <div class="mt-5">
+                        <button
+                          type="button"
+                          class="inline-flex items-center justify-center py-2 px-3 rounded-xl bg-white text-gray-800 hover:text-green-500 text-sm font-semibold transition"
+                        >
+                          Start tracking
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="p-4 bg-yellow-100 rounded-xl text-gray-800">
+                    <div class="font-bold text-2xl leading-none">20</div>
+                    <div class="mt-2">Tasks finished</div>
+                  </div>
+                  <div class="p-4 bg-yellow-100 rounded-xl text-gray-800">
+                    <div class="font-bold text-2xl leading-none">5,5</div>
+                    <div class="mt-2">Tracked hours</div>
+                  </div>
+                  <div class="col-span-2">
+                    <div class="p-4 bg-purple-100 rounded-xl text-gray-800">
+                      <div class="font-bold text-xl leading-none">
+                        Your daily plan
+                      </div>
+                      <div class="mt-2">5 of 8 completed</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <h2 class="text-2xl font-bold mb-4">Your tasks today</h2>
+
+                <div class="space-y-4">
+                  <div class="p-4 bg-white border rounded-xl text-gray-800 space-y-2">
+                    <div class="flex justify-between">
+                      <div class="text-gray-400 text-xs">Number 10</div>
+                      <div class="text-gray-400 text-xs">4h</div>
+                    </div>
+                    <a
+                      href="javascript:void(0)"
+                      class="font-bold hover:text-yellow-800 hover:underline"
+                    >
+                      Blog and social posts
+                    </a>
+                    <div class="text-sm text-gray-600">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="1em"
+                        height="1em"
+                        fill="currentColor"
+                        class="text-gray-800 inline align-middle mr-1"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                      </svg>
+                      Deadline is today
+                    </div>
+                  </div>
+                  <div class="p-4 bg-white border rounded-xl text-gray-800 space-y-2">
+                    <div class="flex justify-between">
+                      <div class="text-gray-400 text-xs">Grace Aroma</div>
+                      <div class="text-gray-400 text-xs">7d</div>
+                    </div>
+                    <a
+                      href="javascript:void(0)"
+                      class="font-bold hover:text-yellow-800 hover:underline"
+                    >
+                      New campaign review
+                    </a>
+                    <div class="text-sm text-gray-600">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="1em"
+                        height="1em"
+                        fill="currentColor"
+                        class="text-gray-800 inline align-middle mr-1"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                      </svg>
+                      New feedback
+                    </div>
+                  </div>
+                  <div class="p-4 bg-white border rounded-xl text-gray-800 space-y-2">
+                    <div class="flex justify-between">
+                      <div class="text-gray-400 text-xs">Petz App</div>
+                      <div class="text-gray-400 text-xs">2h</div>
+                    </div>
+                    <a
+                      href="javascript:void(0)"
+                      class="font-bold hover:text-yellow-800 hover:underline"
+                    >
+                      Cross-platform and browser QA
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
