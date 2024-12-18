313df9c31e6dbe468a934af3828e4189a6dcc5c0
habibmy <habib.my@rediffmail.com>
2024-12-18 23:55:58 +0530

    feat: pagination in payments table

--- src/app/payments/page.jsx
+++ src/app/payments/page.jsx
@@ -2,20 +2,23 @@ import React from "react";
 import { DataTable } from "./data-table";
 import { columns } from "./columns";
 import { getPayments } from "@/services/dbService";
+import PaginationComponent from "@/components/pagination";
 
-const page = async () => {
-  let data = [];
-  try {
-    data = await getPayments();
-    console.log(data);
-  } catch (error) {
-    console.error(error);
-  }
+const page = async ({ searchParams }) => {
+  const page = searchParams.page ? parseInt(searchParams.page, 10) : 1;
+  const pageSize = 10;
+
+  const { payments, totalPayments } = await getPayments(page, pageSize);
+
+  const totalPages = Math.ceil(totalPayments / pageSize);
 
   return (
     <div>
       <h2 className="text-3xl font-serif mb-3">Payments</h2>
-      <DataTable columns={columns} data={data} />
+      <DataTable columns={columns} data={payments} />
+
+      {/* Pagination Controls */}
+      <PaginationComponent page={page} totalPages={totalPages} />
     </div>
   );
 };
