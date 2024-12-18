1fa8ff2b3bfeb940f6601315936ef64719fba269
habibmy <habib.my@rediffmail.com>
2024-12-18 23:55:51 +0530

    feat: pagination in sales table

--- src/app/sales/page.jsx
+++ src/app/sales/page.jsx
@@ -2,19 +2,23 @@ import React from "react";
 import { DataTable } from "./data-table";
 import { columns } from "./columns";
 import { getSales } from "@/services/dbService";
+import PaginationComponent from "@/components/pagination";
 
-const page = async () => {
-  let data = [];
-  try {
-    data = await getSales();
-  } catch (error) {
-    console.error(error);
-  }
+const page = async ({ searchParams }) => {
+  const page = searchParams.page ? parseInt(searchParams.page, 10) : 1;
+  const pageSize = 10;
+
+  const { sales, totalSales } = await getSales(page, pageSize);
+
+  const totalPages = Math.ceil(totalSales / pageSize);
 
   return (
     <div>
-      <h2 className="text-3xl font-serif mb-3">All Sales</h2>
-      <DataTable columns={columns} data={data} />
+      <h2 className="text-3xl font-serif mb-3">Sales</h2>
+      <DataTable columns={columns} data={sales} />
+
+      {/* Pagination Controls */}
+      <PaginationComponent page={page} totalPages={totalPages} />
     </div>
   );
 };
