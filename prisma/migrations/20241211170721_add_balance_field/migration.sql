6890cd50cf7231772b8a78fb02453d9f4c07547b
habibmy <habib.my@rediffmail.com>
2024-12-22 17:00:56 +0530

    feat: refactor database schema by removing obsolete migration files and adding products table with relationships

--- prisma/migrations/20241211170721_add_balance_field/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "customers" ADD COLUMN     "balance" DOUBLE PRECISION NOT NULL DEFAULT 0;
--- prisma/migrations/20241222073923_init/migration.sql
+++ prisma/migrations/20241222073923_init/migration.sql
@@ -1,14 +1,43 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE "sales" (
+    "id" SERIAL NOT NULL,
+    "description" TEXT,
+    "amount" MONEY NOT NULL,
+    "size" INTEGER NOT NULL,
+    "quantity" DOUBLE PRECISION NOT NULL,
+    "rate" MONEY NOT NULL,
+    "customerId" INTEGER NOT NULL,
+    "date" TIMESTAMPTZ(3) NOT NULL,
+    "createdAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ(3) NOT NULL,
 
-  - You are about to drop the `payments` table. If the table is not empty, all the data it contains will be lost.
+    CONSTRAINT "sales_pkey" PRIMARY KEY ("id")
+);
 
-*/
--- DropForeignKey
-ALTER TABLE "payments" DROP CONSTRAINT "payments_customerId_fkey";
+-- CreateTable
+CREATE TABLE "customers" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "address" TEXT,
+    "phone" TEXT,
+    "createdAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ(3) NOT NULL,
+    "balance" DOUBLE PRECISION NOT NULL DEFAULT 0,
 
--- DropTable
-DROP TABLE "payments";
+    CONSTRAINT "customers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "rates" (
+    "id" SERIAL NOT NULL,
+    "size" INTEGER NOT NULL,
+    "rate" MONEY NOT NULL,
+    "customerId" INTEGER,
+    "createdAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ(3) NOT NULL,
+
+    CONSTRAINT "rates_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "customer_payments" (
@@ -62,6 +91,24 @@ CREATE TABLE "vendor_payments" (
     CONSTRAINT "vendor_payments_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "products" (
+    "id" SERIAL NOT NULL,
+    "purchaseId" INTEGER NOT NULL,
+    "product" TEXT NOT NULL,
+    "quantity" DOUBLE PRECISION NOT NULL,
+    "rate" DOUBLE PRECISION NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "sales" ADD CONSTRAINT "sales_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "customers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "rates" ADD CONSTRAINT "rates_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "customers"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
 -- AddForeignKey
 ALTER TABLE "customer_payments" ADD CONSTRAINT "customer_payments_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "customers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -70,3 +117,6 @@ ALTER TABLE "purchases" ADD CONSTRAINT "purchases_vendorId_fkey" FOREIGN KEY ("v
 
 -- AddForeignKey
 ALTER TABLE "vendor_payments" ADD CONSTRAINT "vendor_payments_vendorId_fkey" FOREIGN KEY ("vendorId") REFERENCES "vendors"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "products" ADD CONSTRAINT "products_purchaseId_fkey" FOREIGN KEY ("purchaseId") REFERENCES "purchases"("id") ON DELETE CASCADE ON UPDATE CASCADE;
